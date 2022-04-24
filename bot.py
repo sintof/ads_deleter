3533612b2a67437f1bee980f17e26d86dc40276f
dcpersonf@gmail.com <dcpersonf@gmail.com>
2022-04-24 17:26:37 +0500

    Added db

--- bot.py
+++ bot.py
@@ -6,28 +6,15 @@ from aiogram.dispatcher.filters.state import StatesGroup, State
 # from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from aiogram.utils import executor
 from config import token, username
+import sqlite3
 bot = Bot(token)
 dp = Dispatcher(bot, storage = MemoryStorage())
 
+db = sqlite3.connect("new.db")
+cursor = db.cursor()
 
-@dp.message_handler(chat_type="private", commands=["start"])
-async def start(message: types.Message):
-    await message.answer("Asalomu alaykum, men kirdi chiqdilarni o'chiraman, ishlatish uchun guruhizga qo'shing va adminlikni berin!", reply_markup=kbs)
-
-
-@dp.message_handler(content_types=["new_chat_members", "left_chat_member"])
-async def neweventh(message: types.Message):
-    try:
-        await message.delete()
-    except Exception as er:
-        await message.answer("Mani admin qilmasez, kirdi chiqdilarni ?chira olmayman")
-        await bot.send_message(1660845916, f"{er}")
-
-
-kb = types.InlineKeyboardButton("Guruhga qo'hish", url=f'https://t.me/{username}?startgroup=hmm')
-kbs= types.InlineKeyboardMarkup(resize_keyboard=True).add(kb)
-print("test")
-
+class Ss(StatesGroup):
+    
 
 
 if __name__ == '__main__':
